3fbc7e749e4be236d26fc2c2fcd13bbddff6e51e
Paul Kourany <pkourany@users.noreply.github.com>
2016-06-06 22:21:58 -0400

    Update dmd_demo.ino

--- firmware/examples/dmd_demo.ino
+++ firmware/examples/dmd_demo.ino
@@ -35,14 +35,12 @@
  The DMD library is open source (GPL), for more see DMD.cpp and DMD.h
 
 --------------------------------------------------------------------------------------*/
-
+!!! The SparkIntervalTimer library MUST be added to this example !!!
 /*--------------------------------------------------------------------------------------
   Includes
 --------------------------------------------------------------------------------------*/
 //#include <SPI.h>        //SPI.h must be included as DMD is written by SPI (the IDE complains otherwise)
 #include "DMD.h"        //
-//#include <TimerOne.h>   //
-#include "SparkIntervalTimer.h"
 #include "SystemFont5x7.h"
 #include "Arial_black_16.h"
 
